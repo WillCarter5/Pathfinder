b00747c36c588141b3bd55853719024c65d76fea
WillCarter5 <will@carter.pro>
2025-12-18 13:19:26 -0500

    Minor updates

--- src/pathfinder.py
+++ src/pathfinder.py
@@ -1,46 +1,13 @@
 #!/usr/bin/env python3
 from math import sqrt, radians, sin, cos, sqrt, atan2
 from heapq import heappop, heappush
-from typing import Iterator
 
-import sys
 import os
 import argparse
 
-import json
 import geopandas as gpd
 import networkx as nx
 import matplotlib.pyplot as plt
-
-from shapely.geometry import box
-
-POIs = {
-	"SDH": (41.699760781572174, -86.24147043536637),
-	"NDH": (41.70437014837382, -86.2364442805955),
-	"LaFun": (41.70191117489054, -86.23817641779294),
-	"Basilica": (41.702233976093076, -86.23971507342385),
-	"Dome": (41.70258020555431, -86.23897055032573),
-	"DeBartolo": (41.69831370272931, -86.2366903408291),
-	"Stadium": (41.699501279617564, -86.23473228991755),
-	"HesburghSide": (41.70239708517827, -86.23473668849877),
-	"Hesburgh": (41.70219366518085, -86.23413016186146),
-	"McGlinn": (41.69829764944114, -86.2424898586523),
-	"Alumni": (41.699900943856946, -86.23929903601268),
-	"Flaherty": (41.70332850891336, -86.23313476603498),
-	"Morris_Inn": (41.6981843865698, -86.23907795393022),
-	"Bookstore": (41.696540162518055, -86.23956278553996),
-	"Duncan_Student_Center": (41.69898121029417, -86.23551395664241),
-	"Grotto": (41.70306758520429, -86.24034907784774),
-	"Jordan": (41.70019136394629, -86.23185156359264),
-	"Hayes_Healey": (41.70081369838371, -86.23798014410093),
-	"Pasquerilla_Center": (41.701727856154925, -86.23166423068008),
-	"Jenkins_Nanovic": (41.69586805609035, -86.23796296302217),
-	"Fitzpatrick": (41.69959223372997, -86.2371374802066),
-	"Oshag": (41.70001938119111, -86.23616269421899),
-	"The_Rock": (41.70001584718734, -86.24368922161776),
-	"Lake_Lot": (41.70028557898602, -86.24577267420236),
-	"Bulla_Lot": (41.70357025004068, -86.22777713590293)
-}
 
 def read_pois(filename):
 	pois = dict()
@@ -106,13 +73,6 @@ def djikstra(G, start, end):
 
 	return dists[end], path
 
-def usage():
-	print(
-f"""
-usage: {os.path.basename()} [G]
-"""
-		)
-
 def main():
 	# Create argparser
 	parser = argparse.ArgumentParser(description="Pathfinder script to process data and find paths.")
@@ -137,7 +97,7 @@ def main():
 	G = nx.Graph()
 
 	# Populate graph
-	for idx, row in gdf.iterrows():
+	for _, row in gdf.iterrows():
 		coords = list(row.geometry.coords)
 		
 		for i in range(len(coords)-1):
@@ -178,7 +138,7 @@ def main():
 	print(f"Length: {length}")
 	
 	# MPL output
-	fig, ax = plt.subplots(figsize=(10,10))
+	_, ax = plt.subplots(figsize=(10,10))
 
 	# Plot all edges
 	for u, v in G.edges:
